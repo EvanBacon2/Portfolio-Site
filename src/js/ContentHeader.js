05448c54ffd4646bd1fe22e132f319469ed18d33
Evan Bacon <31549754+EvanBacon2@users.noreply.github.com>
2021-08-02 14:32:17 -0700

    fixed issue where content header wasn't centered correctly

--- src/js/ContentHeader.js
+++ src/js/ContentHeader.js
@@ -6,11 +6,7 @@ export default class ContentHeader extends Component {
     render() {
         return (
             <div id='header-container'>
-                <div class='extra-padding'>
-                    <span id='content-title' class={this.props.theme}>
-                        {this.props.title}
-                    </span>
-                </div>
+                <div id='content-title' class={this.props.theme}>{this.props.title}</div>
                 <hr id='content-underline' class={this.props.theme + ' ' + this.props.fadeAlignment}/>
             </div>
         );
