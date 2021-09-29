02ecbaa731983296ed646c27e4df2c98b0e82d4b
Evan Bacon <31549754+EvanBacon2@users.noreply.github.com>
2021-09-28 19:26:53 -0700

    small changes to fix issue where site would show up as a blank white page

--- src/js/ContentHeader.js
+++ src/js/ContentHeader.js
@@ -3,7 +3,7 @@ import 'css/ContentHeader.css';
 export default function ContentHeader(props) {
     return (
         <div class={'header-container ' + props.state}>
-            <div class={'content-title ' + props.theme}>{props.title}</div>
+            <p class={'content-title ' + props.theme}>{props.title}</p>
             <hr class={'content-underline ' + props.theme + ' ' + props.fadeAlignment}/>
         </div>
     );
