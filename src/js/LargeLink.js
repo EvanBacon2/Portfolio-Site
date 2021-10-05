06cb88f68b769f394b4f5cad935b4314a045af96
Evan Bacon <31549754+EvanBacon2@users.noreply.github.com>
2021-10-05 03:26:10 -0700

    converted LargeLink.css to sass

--- src/js/LargeLink.js
+++ src/js/LargeLink.js
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 
-import SvgButton from './SvgButton.js'
+import SvgButton from 'js/SvgButton'
 import FigmaButton from 'js/FigmaButton'
 
-import '../css/LargeLink.css'
+import 'stylesheets/LargeLink.css'
 
-import {ReactComponent as GithubIcon} from '../svg/github.svg'
-import {ReactComponent as WebsiteIcon} from '../svg/website.svg'
-import {ReactComponent as AppStoreIcon} from '../svg/appstore.svg'
-import {ReactComponent as LinkedinIcon} from '../svg/linkedin.svg'
-import {ReactComponent as MailIcon} from '../svg/mail.svg'
+import {ReactComponent as GithubIcon} from 'svg/github.svg'
+import {ReactComponent as WebsiteIcon} from 'svg/website.svg'
+import {ReactComponent as AppStoreIcon} from 'svg/appstore.svg'
+import {ReactComponent as LinkedinIcon} from 'svg/linkedin.svg'
+import {ReactComponent as MailIcon} from 'svg/mail.svg'
 
 const LargeLink = ({state, destination, url}) => {
     const [active, setActive] = useState('')
