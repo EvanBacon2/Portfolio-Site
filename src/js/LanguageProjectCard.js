9f7b115c5fb4a95123292af6303dd529c8911da8
Evan Bacon <31549754+EvanBacon2@users.noreply.github.com>
2021-10-07 19:48:53 -0700

    small changes to fix various safari issues

--- src/js/LanguageProjectCard.js
+++ src/js/LanguageProjectCard.js
@@ -7,7 +7,7 @@ const LanguageProjectCard = ({showcaseCallback}) => {
     return (
         <ProjectCard title='Language Buddy' theme='language' type='ux' cardPhoto='Language_Card_Photo.png'
                      description='A meet up/study app for international students' showcaseCallback={showcaseCallback}>
-            <SmallLink theme='language' url='https://www.figma.com/file/NoxMY1g6x97M8WzxnpFUmL/Language-Buddy'><FigmaIcon/></SmallLink>
+            <SmallLink theme='language' size='figma' url='https://www.figma.com/file/NoxMY1g6x97M8WzxnpFUmL/Language-Buddy'><FigmaIcon/></SmallLink>
         </ProjectCard>
     )
 }
--- src/js/SmallLink.js
+++ src/js/SmallLink.js
@@ -2,14 +2,14 @@ import * as React from 'react'
 
 import 'stylesheets/SmallLink.css'
 
-const SmallLink = ({ theme, url, children }) => {
+const SmallLink = ({ theme, size, url, children }) => {
     const injectStyle = (newClass) =>{
         return React.Children.map(children, child => 
             React.cloneElement(child, { className: child.props.className + ' ' + newClass}))
     }
 
     return (
-        <a className={'small-link-box ' + theme} href={url} target='_blank' rel="noreferrer">
+        <a className={'small-link-box ' + theme + ' ' + size} href={url} target='_blank' rel="noreferrer">
             {injectStyle('small-link ' + theme)}
         </a>
     )
